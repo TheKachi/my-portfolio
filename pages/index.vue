239d0019aa8b1bd009ed8d5a98fda43732e60f4d
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-02-07 11:30:57 +0100

    New favicon

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="flex justify-between items-center px-12 md:px-48">
       <span class="text-3xl">🦄</span>
-      <div class="flex justify-end gap-16 items-center">
+      <!-- <div class="flex justify-end gap-16 items-center">
         <img src="../assets/img/hamburger.svg" alt="" />
         <img src="../assets/img/moon.svg" alt="" />
-      </div>
+      </div> -->
     </div>
     <Hero />
     <Portfolio />
