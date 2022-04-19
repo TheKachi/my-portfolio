b67cdfe7c98898fe4a19f01887f57aa9089d77cd
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-04-20 00:30:27 +0100

    add color mode, delete unecessary comments

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,41 +23,27 @@ export default {
     ],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss",
-  ],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/pwa
-    "@nuxtjs/pwa",
-    // https://go.nuxtjs.dev/content
-    "@nuxt/content",
+  modules: ["@nuxtjs/pwa", "@nuxt/content"],
 
-    // " @nuxtjs/svg",
-  ],
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: "en",
     },
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  colorMode: {
+    classSuffix: "",
+  },
 };
