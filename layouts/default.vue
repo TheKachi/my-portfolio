a537300c5dba46c18ddb1a5e5ce192abcbf91dd0
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-04-20 09:43:16 +0100

    layout edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-12 gap-0">
-    <side-bar class="col-span-1" />
-    <Nuxt class="col-span-11 px-10 py-36 bg-white" />
+    <side-bar class="hidden md:block md:col-span-1" />
+    <Nuxt class="col-span-12 md:col-span-11 px-10 py-36" />
   </div>
 </template>
 
