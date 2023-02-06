a67e806edd73534d8c40508000b443dbc99e4ffc
Dikachi Nze <nzebenflorence@gmail.com>
2023-02-06 01:29:04 +0100

    add navbar to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="grid grid-cols-12 gap-0">
-    <side-bar class="hidden md:block md:col-span-1" />
-    <Nuxt class="col-span-12 w-full md:col-span-11 py-36" />
+  <div class="p-12 md:py-24 md:px-48">
+    <Navbar />
+    <div class="grid grid-cols-12 gap-0">
+      <side-bar class="hidden md:block md:col-span-1" />
+      <Nuxt class="col-span-12 w-full md:col-span-11 py-36" />
+    </div>
   </div>
 </template>
 
 <script>
 import SideBar from "@/components/SideBar";
+import Navbar from "../components/Navbar.vue";
 
 export default {
   components: {
     SideBar,
+    Navbar,
   },
 };
 </script>
