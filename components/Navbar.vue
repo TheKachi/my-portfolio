d16349564016cbbdaddcfea2fed3a6145b7a9317
Dikachi Nze <nzebenflorence@gmail.com>
2023-02-06 01:26:15 +0100

    create navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="flex px-36">
+    <nuxt-link to="/" class="no-underline font-bold">DN</nuxt-link>
+    <!-- theme toggler  -->
+    <ThemeToggler />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped></style>
