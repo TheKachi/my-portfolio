7510027fb97a859e6147c954d84f4b670d6e96bb
Dikachi Nze <nzebenflorence@gmail.com>
2023-02-06 01:25:38 +0100

    add carousel

---
+++ components/Carousel.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app class="h-[530px] object-cover">
+    <v-carousel cycle interval="3000" height="530" hide-delimiter-background>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
+      </v-carousel-item>
+    </v-carousel>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  props: {
+    slides: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
