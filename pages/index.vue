3688adf9923cd915e199421bc10df00d9937c6e2
Dikachi Nze <nzebenflorence@gmail.com>
2023-02-06 01:30:14 +0100

    remove toggler from index

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <div class="flex justify-end px-12 md:px-48">
-      <ThemeToggler />
-    </div>
     <Hero />
-    <Portfolio />
+    <PortfolioSection />
     <Contact />
   </div>
 </template>
 
 <script>
-import ThemeToggler from "../components/ThemeToggler.vue";
 export default {
   name: "IndexPage",
-  components: { ThemeToggler },
 };
 </script>
