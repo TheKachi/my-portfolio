573fd78cfe3b940cd4fcf4a07aef22e2bd4bfaaf
Dikachi Nze <nzebenflorence@gmail.com>
2023-02-07 18:05:20 +0100

    add back to projects button

--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="px-0 sm:px-20 pt-[150px] pb-60">
+  <section class="px-0 sm:px-20 pt-[150px] pb-60" id="projects">
     <h2 class="mb-80 text-center text-xl sm:text-2xl md:text-3xl font-bold">
       Some of the things I've built...
     </h2>
